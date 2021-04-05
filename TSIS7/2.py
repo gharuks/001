1ee18275e9aca1b3748f32880cc1d49da8794da8
gharuks <akoishybay@mail.ru>
2021-04-05 21:41:02 +0600

    pygame

--- TSIS7/2.py
+++ TSIS7/2.py
@@ -11,8 +11,7 @@ BLUE = (0, 0, 255)
 running = True
 while running:
   for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    if event.type == pygame.QUIT: running = False
   screen.fill(WHITE)
   pygame.draw.rect(screen, BLACK, (90, 86, 620, 428), 2)
   pygame.draw.line(screen, BLACK, (90, 300), (710, 300), 2)
@@ -23,16 +22,15 @@ while running:
     if step==496:
       pygame.draw.line(screen, BLACK, (step, 86), (step, 108), 1)
       pygame.draw.line(screen, BLACK, (step, 156), (step, 514), 1)
-    else:
-      pygame.draw.line(screen, BLACK, (step, 86), (step, 514), 1)
+    else: pygame.draw.line(screen, BLACK, (step, 86), (step, 514), 1)
   font=pygame.font.SysFont("Times New Roman", 18, italic=True)
   t1=font.render('sin(x)', True, BLACK)
   t2=font.render('cos(x)', True, BLACK)
   screen.blit(t1, (476, 111))
   screen.blit(t2, (476, 131))
-  pygame.draw.line(screen, RED, (525, 122), (565, 122), 2)
-  for step in range(525, 557, 13):
-    pygame.draw.line(screen, BLUE, (step, 142), (step+10, 142), 2)
+  pygame.draw.line(screen, RED, (525, 122), (565, 122))
+  for step in range(525, 565, 8):
+    pygame.draw.line(screen, BLUE, (step, 142), (step+5, 142))
 
   for step in range(132, 492, 24):
     pygame.draw.line(screen, BLACK, (90, step), (100, step), 1)
@@ -63,21 +61,16 @@ while running:
   dis=0
   while a>=-0.75:
       text=font.render(str(a), True, BLACK)
-      if a>0:
-        screen.blit(text, (40, 146+dis))
-      else:
-        screen.blit(text, (33, 146+dis))
+      if a>0: screen.blit(text, (40, 146+dis))
+      else: screen.blit(text, (33, 146+dis))
       dis+=96
       a-=0.50
-  
   poi=['1.00', '0.50', '0.00', '-1.00', '-0.50']
   dis=0
   for x in poi:
       text=font.render(x, True, BLACK)
-      if len(x)<=4:
-        screen.blit(text, (40, 98+dis))
-      else:
-        screen.blit(text, (33, 98+dis))
+      if len(x)<=4: screen.blit(text, (40, 98+dis))
+      else: screen.blit(text, (33, 98+dis))
       dis+=96
   b=-3
   dis=0
@@ -97,55 +90,49 @@ while running:
             screen.blit(text, (100+dis, 525))
       elif b%1==0.5:
           if b==0.5:
-            text=font.render("π/2", True, BLACK)
-            screen.blit(text, (100+dis, 525))
+            text=font.render("π", True, BLACK)
+            screen.blit(text, (105+dis, 525))
+            l=font.render('__', True, BLACK)
+            screen.blit(l, (100+dis, 525))
+            u=font.render('2', True, BLACK)
+            screen.blit(u, (105+dis, 545)) 
           elif b==-0.5:
-            text=font.render("-π/2", True, BLACK)
-            screen.blit(text, (93+dis, 525))
+            text=font.render("-π", True, BLACK)
+            screen.blit(text, (100+dis, 525))
+            l=font.render('__', True, BLACK)
+            screen.blit(l, (100+dis, 525))
+            u=font.render('2', True, BLACK)
+            screen.blit(u, (103+dis, 545))
           elif b<0:
-            text=font.render(str(int(b*2))+"π/2", True, BLACK)
-            screen.blit(text, (89+dis, 525))
+            text=font.render(str(int(b*2))+"π", True, BLACK)
+            screen.blit(text, (93+dis, 525))
+            l=font.render('__', True, BLACK)
+            screen.blit(l, (99+dis, 525))
+            u=font.render('2', True, BLACK)
+            screen.blit(u, (103+dis, 545))
           elif b>0:
-            text=font.render(str(int(b*2))+"π/2", True, BLACK)
-            screen.blit(text, (92+dis, 525))            
+            text=font.render(str(int(b*2))+"π", True, BLACK)
+            screen.blit(text, (100+dis, 525))  
+            l=font.render('__', True, BLACK)
+            screen.blit(l, (100+dis, 525))
+            u=font.render('2', True, BLACK)
+            screen.blit(u, (103+dis, 545))       
       dis+=48
       b+=0.5
   text=font.render('0', True, BLACK)
   screen.blit(text, (395, 525))
   font=pygame.font.SysFont("Times New Roman", 30, italic=True)
   text=font.render('x', True, BLACK)
-  screen.blit(text, (395, 555))
+  screen.blit(text, (390, 555))
   for dis in range(0,3):
-      font=pygame.font.SysFont("Times New Roman", 15, italic=True)
-      t=font.render(str(-3+dis), True, BLACK)
-      screen.blit(t, (113+dis*80, 327))
-  
-  x=-3*math.pi
-  dx=math.pi/1000
-  x0=112
-  dx0=1/10.4
-  p=[]
-  while x0<=688:
-      poi=(x0, 300-192*math.cos(x))
-      p.append(poi)
-      x0+=dx0
-      x+=dx
-  pygame.draw.aalines(screen, BLUE, False, p)
-    
-  # x=-3*math.pi
-  # dx=math.pi/1000 
-  # x0=112
-  # dx0=1/10.4
-  # p=[]
-  # while x0<=688:
-  #     poi=(x0, 300-192*math.sin(x))
-  #     p.append(poi)
-  #     x0+=dx0
-  #     x+=dx
-  p=[]
-  for x in range(0,6):
-      poi=(112+x*1/10.4, 300-192*math.sin(-3*math.pi+x*math.pi/1000))
-      p.append(poi)
-  pygame.draw.aalines(screen, RED, False, p)
+    font=pygame.font.SysFont("Times New Roman", 15, italic=True)
+    t=font.render(str(-3+dis), True, BLACK)
+    screen.blit(t, (113+dis*80, 327))
+  #graphofcos
+  for x in range(112,688,3):
+    pygame.draw.aalines(screen, BLUE, False, [(x, 300+192*math.cos((x-112)/96*math.pi)),(x+1, 300+192*math.cos((x-111)/96*math.pi))])
+ #graphofsin
+  for i in range(112,688):
+    pygame.draw.aalines(screen, RED, False, [(i, 300+192*math.sin((i-112)/96*math.pi)),(i+1, 300+192*math.sin((i-111)/96*math.pi))])
   pygame.display.flip()
 pygame.quit()
